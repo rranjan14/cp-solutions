9a83167a4d7f782f8247177915cce50808251d87
Rahul Ranjan <rranjan18bcs@iiitkottayam.ac.in>
2021-02-23 11:56:04 +0530

    solutions to some dynamic programming tagged easy problems

---
+++ LeetCode/53. Maximum Subarray.cpp
@@ -0,0 +1,42 @@
+/*
+
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example 1:
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+Example 2:
+Input: nums = [1]
+Output: 1
+
+Example 3:
+Input: nums = [0]
+Output: 0
+
+Example 4:
+Input: nums = [-1]
+Output: -1
+
+Example 5:
+Input: nums = [-100000]
+Output: -100000
+
+Constraints:
+1 <= nums.length <= 3 * 104
+-105 <= nums[i] <= 105
+
+*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        long int sum=0,ans=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            sum = sum>nums[i]?sum:nums[i];
+            ans = ans>sum?ans:sum;
+        }
+        return ans;
+    }
+};
